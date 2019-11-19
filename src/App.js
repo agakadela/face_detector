e0ce34271b5e2f2dc192f15047837d8f215eb5ad
woziu <agn.woz@gmail.com>
2019-11-19 19:41:09 +0100

    added some styling and components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 360,
+      density: {
+        enable: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        speed: 4,
+        size_min: 0.3
+      }
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      random: true,
+      speed: 1,
+      direction: "top",
+      out_mode: "out"
+    }
+  }
+};
+class App extends React.Component {
+  state = { input: "" };
+
+  inputChangeHandler = event => {
+    console.log(event);
+  };
+
+  submitHandler = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          inputChangeHandler={this.inputChangeHandler}
+          submitHandler={this.submitHandler}
+        />
+        {/* 
+      <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div className="white f3">
+      {"Aga, your current rank is..."}
+      <div className="white f3">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
